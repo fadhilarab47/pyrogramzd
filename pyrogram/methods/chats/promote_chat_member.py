c0a6e12354475e446a668afb655334f27594ab9c
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-07-06 21:56:53 +0700

    update pyrogram/methods/chats/promote_chat_member.py

--- pyrogram/methods/chats/promote_chat_member.py
+++ pyrogram/methods/chats/promote_chat_member.py
@@ -1,22 +1,23 @@
-#  Pyrogram - Telegram MTProto API Client Library for Python
+#  Pyrofork - Telegram MTProto API Client Library for Python
 #  Copyright (C) 2017-present Dan <https://github.com/delivrance>
+#  Copyright (C) 2022-present Mayuri-Chan <https://github.com/Mayuri-Chan>
 #
-#  This file is part of Pyrogram.
+#  This file is part of Pyrofork.
 #
-#  Pyrogram is free software: you can redistribute it and/or modify
+#  Pyrofork is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU Lesser General Public License as published
 #  by the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
 #
-#  Pyrogram is distributed in the hope that it will be useful,
+#  Pyrofork is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU Lesser General Public License for more details.
 #
 #  You should have received a copy of the GNU Lesser General Public License
-#  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
+#  along with Pyrofork.  If not, see <http://www.gnu.org/licenses/>.
 
-from typing import Union
+from typing import Union, Optional
 
 import pyrogram
 from pyrogram import raw, types, errors
@@ -24,10 +25,11 @@ from pyrogram import raw, types, errors
 
 class PromoteChatMember:
     async def promote_chat_member(
-        self: "pyrogram.Client",
-        chat_id: Union[int, str],
-        user_id: Union[int, str],
-        privileges: "types.ChatPrivileges" = None,
+            self: "pyrogram.Client",
+            chat_id: Union[int, str],
+            user_id: Union[int, str],
+            privileges: "types.ChatPrivileges" = None,
+            title: Optional[str] = "",
     ) -> bool:
         """Promote or demote a user in a supergroup or a channel.
 
@@ -39,13 +41,20 @@ class PromoteChatMember:
         Parameters:
             chat_id (``int`` | ``str``):
                 Unique identifier (int) or username (str) of the target chat.
+                You can also use chat public link in form of *t.me/<username>* (str).
 
             user_id (``int`` | ``str``):
                 Unique identifier (int) or username (str) of the target user.
                 For a contact that exists in your Telegram address book you can use his phone number (str).
+                You can also use user profile link in form of *t.me/<username>* (str).
 
             privileges (:obj:`~pyrogram.types.ChatPrivileges`, *optional*):
                 New user privileges.
+
+            title: (``str``, *optional*):
+                A custom title that will be shown to all members instead of "Owner" or "Admin".
+                Pass None or "" (empty string) will keep the current title.
+                If you want to delete the custom title, use :meth:`~pyrogram.Client.set_administrator_title()` method.
 
         Returns:
             ``bool``: True on success.
@@ -73,9 +82,10 @@ class PromoteChatMember:
         except errors.RPCError:
             raw_chat_member = None
 
-        rank = None
-        if isinstance(raw_chat_member, raw.types.ChannelParticipantAdmin):
+        if not title and isinstance(raw_chat_member, raw.types.ChannelParticipantAdmin):
             rank = raw_chat_member.rank
+        else:
+            rank = title
 
         await self.invoke(
             raw.functions.channels.EditAdmin(
@@ -92,9 +102,13 @@ class PromoteChatMember:
                     pin_messages=privileges.can_pin_messages,
                     add_admins=privileges.can_promote_members,
                     manage_call=privileges.can_manage_video_chats,
+                    manage_topics=privileges.can_manage_topics,
+                    post_stories=privileges.can_post_stories,
+                    edit_stories=privileges.can_edit_stories,
+                    delete_stories=privileges.can_delete_stories,
                     other=privileges.can_manage_chat
                 ),
-                rank=rank or ""
+                rank=rank
             )
         )
 
